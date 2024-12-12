445d6f27a08ca9f5275bc7102cbb7209737ed780
SkalapEnder <skalap2endra@gmail.com>
2024-12-12 16:16:09 +0500

    Update NumericalMethod.py

--- NumericalMethod.py
+++ NumericalMethod.py
@@ -13,11 +13,14 @@ def error_tolerance(precision, x1, x2):
 
 def bisection_method(a, b, precision, max_iters):
     result_list = []
+    error_list = []
     iters = 0
 
     while error_tolerance(precision, a, b) and iters < max_iters:
         x = (a + b) * 0.5
+        err = abs(x - b)
         result_list.append([iters, x])
+        error_list.append([iters, err])
         
         if f(x) * f(b) < 0:
             a = x
@@ -25,21 +28,25 @@ def bisection_method(a, b, precision, max_iters):
             b = x
         iters += 1
 
-    return result_list
+    return result_list, error_list
 
 
 def main():
-    precision = 1e-10
+    precision = 1e-7
     max_iters = 100
 
     a = float(input("Write a: "))
     b = float(input("Write b: "))
 
-    result_list = bisection_method(a, b, precision, max_iters)
-
+    result_list, error_list = bisection_method(a, b, precision, max_iters+1) 
+    
     for iteration, answer in result_list:
         print(f"Iteration {iteration}: Answer = {answer}")
     outputGraph_AnswerIteration(result_list)
+    
+    for iteration, error in error_list:
+        print(f"Iteration {iteration}: Error = {error}")
+    outputGraph_ErrorIteration(error_list)
     
    
 def outputGraph_AnswerIteration(result_list):
@@ -49,6 +56,16 @@ def outputGraph_AnswerIteration(result_list):
     plt.xlabel('Iteration')
     plt.ylabel('Answer')
     plt.title('Bisection Method: Iteration vs. Answer')
+    plt.grid(True)
+    plt.show()
+
+def outputGraph_ErrorIteration(error_list):
+    iterations, answers = zip(*error_list)  
+
+    plt.plot(iterations, answers, marker='o')
+    plt.xlabel('Iteration')
+    plt.ylabel('Error')
+    plt.title('Bisection Method: Iteration vs. Error')
     plt.grid(True)
     plt.show()
 
